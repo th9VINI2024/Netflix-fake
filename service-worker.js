132df81a55166ef5dffb5ee0c47e869430e9b8d6
th9VINI2024 <161654715+th9VINI2024@users.noreply.github.com>
2026-02-05 19:33:17 -0300

    Implement service worker for caching and fetch handling

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,15 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("netflix-cache").then(cache => {
+      return cache.addAll(["./", "./index.html"]);
+    })
+  );
+});
 
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
